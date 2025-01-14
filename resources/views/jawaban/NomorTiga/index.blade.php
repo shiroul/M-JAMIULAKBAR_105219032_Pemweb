9589f46e57801246acc92b518150f695fd3fc8a1
Ujang-Kimura <Xaverius0002@gmail.com>
2025-01-14 09:33:16 +0800

    Update index.blade.php

--- resources/views/jawaban/NomorTiga/index.blade.php
+++ resources/views/jawaban/NomorTiga/index.blade.php
@@ -7,22 +7,5 @@
     </tbody>
 </table>
 
-<div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <form class="modal-content" method="POST" action="{{ route('event.update') }}">
-            @csrf
-            <div class="modal-header">
-                <h5 class="modal-title" id="addModalLabel">Edit Jadwal</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                
-            </div>
-            <div class="modal-footer">
-                <button type="submit" class="btn btn-primary"> Submit </button>
-            </div>
-        </form>
-    </div>
-</div>
+<!-- Gunakan tag form dibawah ini untuk submit data jadwal yang akan diupdate. Gunakan contoh modal yang sudah dibuat pada nomor 1 dan 2 sebagai referensi -->
+<!-- <form class="modal-content" method="POST" action="{{ route('event.update') }}"> </form> -->
